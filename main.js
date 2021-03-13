8a770c1df45d03e58f111563bad781bc9bdc4e75
AndreyKudinov63 <64890665+AndreyKudinov63@users.noreply.github.com>
2021-03-13 22:51:49 +0400

    мелкие правки

--- main.js
+++ main.js
@@ -22,17 +22,35 @@ document.addEventListener("DOMContentLoaded", () => {
     el.onclick = function () {
       id = el.dataset.id;
       document.querySelector(".intensive-test__questions").innerHTML = "";
-      document.querySelector(".step2-container__item-result_num").textContent = 0;
-      document.querySelector(".step2-container__progress-caption").textContent = `0%`;
-      document.querySelector(".step2-container__progress-bar_green").style.width = `0%`;
-      getTest(id)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          shuffle(data.results);
-          start(data.results);
-        });
+      document.querySelector(".step2-container__item-result_num").textContent = 1;
+      document.querySelector(".step2-container__progress-caption").textContent = `11%`;
+      document.querySelector(".step3-container__progress-bar_green").style.width = `11%`;
+
+      if (document.querySelectorAll(".intensive-puzzle_item.opened").length > 1) {
+        for (let i = 1; i < 9; i++) {
+          document.querySelectorAll(".intensive-puzzle_item")[i].classList.remove("opened");
+        }
+
+        setTimeout(() => {
+          getTest(id)
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              shuffle(data.results);
+              puzzleStart(data.results);
+            });
+        }, 300);
+      } else {
+        getTest(id)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            shuffle(data.results);
+            puzzleStart(data.results);
+          });
+      }
     };
   });
 
@@ -42,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then((data) => {
       shuffle(data.results);
-      start(data.results);
+      puzzleStart(data.results);
     });
 
   document.querySelector(".btn-caption").onclick = function () {
@@ -55,49 +73,63 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function start(response) {
+async function puzzleStart(response) {
   let test = response;
-  let test_step = 0;
   shuffle(test);
-  test = test.slice(0, 5);
-  let answerHTML = "";
-  let $class = "d-block";
-  let cardClass = "intensive-puzzle__item-first_active";
+  test = test.slice(0, 8);
 
-  document.querySelectorAll(".step2-item__title-js")[0].textContent = `${test[0].category}`;
-  document.querySelectorAll(".step2-item__title-js")[1].textContent = `Вопросы из категории ${test[0].category}`;
+  let answerHTML = "";
+  let puzzleHTML = "";
+
+  let $class = "d-block";
+  let cardClass = "intensive-puzzle_item-first_active";
+
+  document.querySelector(".intensive-test__puzzle").innerHTML = `
+    <div class="intensive-puzzle_item opened">
+      <div class="intensive-puzzle_item-first intensive-puzzle_item-first_active">
+        <div class="intensive-puzzle_item-num">1</div>
+      </div>
+      <div class="intensive-puzzle_item-second">
+        <img src="/images/1.jpg" alt="" />
+      </div>
+    </div>`;
+
+  // document.querySelectorAll(".intensive-puzzle_item").forEach(function (item, index) {
+  //   item.classList.remove("opened");
+  // });
+  // document.querySelectorAll(".intensive-puzzle_item")[0].classList.remove("opened");
+
   for (let i = 0; i < test.length; i++) {
     if (i !== 0) {
       $class = "d-none";
       cardClass = "";
     }
-    let answersArray = [test[i].correct_answer, test[i].incorrect_answers[0], test[i].incorrect_answers[1], test[i].incorrect_answers[2]];
-
+    let answersArray = [test[i].correct_answer, test[i].incorrect_answers[0], test[i].incorrect_answers[1]];
     shuffle(answersArray);
     let answers = "";
     let question = test[i];
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 3; j++) {
       let answer = answersArray[j];
       let check = "";
-      // let check = `<img class="intensive-test__input-check-error" src="./images/intensive-test-input-error.svg" alt="">`;
-      if (answer == test[i].correct_answer) {
-        // check = `<img class="intensive-test__input-img intensive-test__input-check-checked" src="./images/intensive-test-input-checked.svg" alt="">`;
+      // let check = `<img class="intensive-test__input-check-error" src="../images/intensive-test-input-error.svg" alt="">`;
+      if (answer.correct) {
+        // check = `<img class="intensive-test__input-img intensive-test__input-check-checked" src="../images/intensive-test-input-checked.svg" alt="">`;
       } else {
-        // check = `<img class="intensive-test__input-img intensive-test__input-check-error" src="./images/intensive-test-input-error.svg" alt="">`;
+        // check = `<img class="intensive-test__input-img intensive-test__input-check-error" src="../images/intensive-test-input-error.svg" alt="">`;
       }
       answerHTML =
         '<label class="intensive-test__input-container question-container__answer-text" data-id="' +
-        i +
+        answer.id +
         '"><div class="intensive-test__input-wrapper intensive-test__input-wrapper"><input type="radio" name="question' +
         i +
-        '" class="input step2__input-checkbox"><div class="answers intensive-test__input-check">' +
+        '" class="input intensive-test__input-checkbox"><div class="answers intensive-test__input-check">' +
         check +
-        '</div></div><span class="intensive-test__input-text answerText question-container__answer step2-container__answer">' +
+        '</div></div><span class="intensive-test__input-text answerText question-container__answer ">' +
         answer +
         "</span></label>";
       answers += answerHTML;
     }
-    let questionHTML = `<div class="intensive-test__question-container question-container ${$class}" data-id=${i} data-id-backend=${i}>
+    let questionHTML = `<div class="intensive-test__question-container question-container ${$class}" data-id="${i}" data-id-backend=${i}>
                                               <div class="question-container__head">
                                                   <div class="question-container__title">
                                                       Вопрос ${i + 1}
@@ -113,123 +145,94 @@ function start(response) {
                                               </div>
                                           </div>`;
     document.querySelector(".intensive-test__questions").innerHTML += questionHTML;
+    puzzleHTML = `<div class="intensive-puzzle_item" data-id="${i}">
+                                                      <div class="intensive-puzzle_item-first ${cardClass}">
+                                                          <div class="intensive-puzzle_item-num">${i + 1}</div>
+                                                      </div>
+                                                      <div class="intensive-puzzle_item-second">
+                                                          <img src="/images/${i + 2}.jpg" alt="">
+                                                      </div>
+                                                  </div>`;
+    document.querySelector(".intensive-test__puzzle").innerHTML += puzzleHTML;
   }
 
-  {
-    let correctAnswersCounter = 0;
-    let falseAnswersCounter = 0;
-    // await checkAnswer(parseInt(question_id)+10, answer_id).then(function(response) {
-    //     console.log('3. response.result.is_correct -', response.result.is_correct);
-    // })
-    document.querySelector(".step2-container__item-result__gray").textContent = `/${5}`;
+  let correctAnswersCounter = 1;
+  document.querySelectorAll(".step3-item__title-js").forEach(function (item, index) {
+    item.textContent = `${test[0].category}`;
+  });
+  document.querySelectorAll(".intensive-test__input-checkbox").forEach(function (item, index) {
+    item.addEventListener("change", async function (e) {
+      let $this = this;
+      let question = $this.closest(".question-container");
+      let answer = $this.closest(".intensive-test__input-container");
+      let answer_id = answer.getAttribute("data-id");
+      let question_id = $this.closest(".question-container").getAttribute("data-id");
+      let question_id_backend = $this.closest(".question-container").getAttribute("data-id-backend");
 
-    document.querySelectorAll(".step2__input-checkbox").forEach(function (item, index) {
-      item.addEventListener("change", function (e) {
-        let $this = this;
-        let question = $this.closest(".question-container");
-        let answer = $this.closest(".intensive-test__input-container");
-        let answer_id = answer.getAttribute("data-id");
-        let question_id = $this.closest(".question-container").getAttribute("data-id");
-        let comment = document.createElement("div");
-        question.querySelectorAll(".question-container__comment").forEach(function (item, index) {
-          item.remove();
+      let comment = document.createElement("div");
+      question.querySelectorAll(".question-container__comment").forEach(function (item, index) {
+        item.remove();
+      });
+      question.querySelectorAll(".question-container__comment_error").forEach(function (item, index) {
+        item.remove();
+      });
+      if (answer.textContent == test[question_id].correct_answer) {
+        correctAnswersCounter++;
+        document.querySelector(".step2-container__item-result_num").textContent = correctAnswersCounter;
+        document.querySelector(".step2-container__progress-caption").textContent = `${Math.round((correctAnswersCounter * 100) / 9)}%`;
+        document.querySelector(".step3-container__progress-bar_green").style.width = `${Math.round((correctAnswersCounter * 100) / 9)}%`;
+        answer.querySelector(".intensive-test__input-check").innerHTML =
+          '<img class="intensive-test__input-img intensive-test__input-check-error" src="/images/intensive-test-input-checked.svg" alt="">';
+        comment.classList.add("question-container__comment");
+        comment.innerHTML = `<div class="question-container__comment-title">Comment correct</div></div><div class="question-container__comment-text">Вопрос уровня ${test[question_id].difficulty}</div>`;
+        answer.classList.add("intensive-test__input-container-correct");
+        document.querySelectorAll(".intensive-test__input-container").forEach(function (item, index) {
+          item.classList.remove("intensive-test__input-container-false");
         });
-        question.querySelectorAll(".question-container__comment_error").forEach(function (item, index) {
-          item.remove();
+        question.querySelectorAll(".intensive-test__input-container").forEach(function (item, index) {
+          item.classList.add("intensive-test__input-container_disable");
         });
-        // for (let i = 0; i < step.length; i++) {
-        //   if (questions_step2[question_id].answers[i].is_correct) {
-        //     question.querySelectorAll(".intensive-test__input-container")[i].classList.add("question-container-js");
-        //   }
-        // }
-        if (answer.textContent == test[question_id].correct_answer) {
-          correctAnswersCounter++;
-          document.querySelector(".step2-container__item-result_num").textContent = correctAnswersCounter;
-          document.querySelector(".step2-container__progress-caption").textContent = `${Math.round((correctAnswersCounter * 100) / 5)}%`;
-          document.querySelector(".step2-container__progress-bar_green").style.width = `${Math.round((correctAnswersCounter * 100) / 5)}%`;
-          answer.querySelector(".intensive-test__input-check").innerHTML =
-            '<img class="intensive-test__input-img intensive-test__input-check-error" src="./images/intensive-test-input-checked.svg" alt="">';
-          // comment.classList.add("question-container__comment_true");
-          // comment.innerText = 'Верно!';
-          comment.classList.add("step2-question-container__comment");
-          comment.innerHTML = `<div class="step2-question-container__comment-title">Верно!</div><div class="question-container__comment-text">Вопрос уровня ${test[question_id].difficulty}</div>`;
-          question.querySelectorAll(".intensive-test__input-container").forEach(function (item, index) {
-            item.classList.add("intensive-test__input-container_disable");
-          });
-          answer.classList.add("intensive-test__input-container-correct");
-          document.querySelectorAll(".intensive-test__input-container").forEach(function (item, index) {
-            item.classList.remove("intensive-test__input-container-false");
-          });
-          // if (correctAnswersCounter == 5) {
-          //     document.querySelector('.step2-container__item-result__gray').style.color = '#061b36'
-          //     document.querySelector('.step2__button_green').onclick = function() {
-          //         document.querySelector(".step2-start").style.display = "none";
-          //         document.querySelector(".step2-end").style.display = "block";
-          //     }
-          // }
-          if (test_step == test.length - 1 && correctAnswersCounter != test.length) {
-            document.querySelector(".step2__button_green").disabled = true;
-          } else {
-            document.querySelector(".step2__button_green").disabled = false;
-          }
-        } else {
-          falseAnswersCounter++;
-          answer.querySelector(".intensive-test__input-check").innerHTML =
-            '<img class="intensive-test__input-img intensive-test__input-check-error" src="./images/intensive-test-input-error.svg" alt="">';
-          // question.querySelectorAll(".intensive-test__input-container").forEach(function (item, index) {
-          //     item.classList.add("intensive-test__input-container_disable");
-          // });
-          // comment.classList.add("question-container__comment");
-          comment.classList.add("question-container__comment_error");
-          comment.classList.add("step2-container__comment_error");
-          // comment.innerHTML = '<div>'+step2[question_id].comment+'</div>';
-          comment.innerText = `Попробуй еще раз!`;
-          document.querySelectorAll(".intensive-test__input-container").forEach(function (item, index) {
-            item.classList.remove("intensive-test__input-container-false");
-          });
-          answer.classList.add("intensive-test__input-container-false");
+        document.querySelector(".intensive-puzzle_item[data-id='" + question_id + "']").classList.add("opened");
+        if (correctAnswersCounter == 9) {
+          document.querySelector(".step2-container__item-result__gray").style.color = "#061b36";
         }
-        answer.after(comment);
-      });
+        if (document.querySelectorAll(".intensive-puzzle_item.opened").length == 9) {
+          setTimeout(function () {
+            document.querySelector(".buttons-wrap").style.display = "none";
+            document.querySelector(".step3-start").style.display = "none";
+            document.querySelector(".step2-end").style.display = "block";
+          }, 1000);
+        }
+      } else {
+        answer.querySelector(".intensive-test__input-check").innerHTML =
+          '<img class="intensive-test__input-img intensive-test__input-check-error" src="/images/intensive-test-input-error.svg" alt="">';
+        comment.innerText = `Попробуй еще раз!`;
+        comment.classList.add("question-container__comment_error");
+        document.querySelectorAll(".intensive-test__input-container").forEach(function (item, index) {
+          item.classList.remove("intensive-test__input-container-false");
+        });
+        answer.classList.add("intensive-test__input-container-false");
+      }
+      answer.after(comment);
     });
-    document.querySelector(".step2__button_green").onclick = function () {
-      document.querySelector(".step2__button_test-back").classList.remove("d-none");
-      document.querySelectorAll(".question-container").forEach(function (item, index) {
-        item.classList.remove("d-block");
-        item.classList.add("d-none");
-      });
-      test_step++;
-      if (test_step == test.length - 1 && correctAnswersCounter != test.length) {
-        document.querySelector(".step2__button_green").disabled = true;
-        document.querySelector(".step2__button_green").textContent = "Завершить тест";
-      } else {
-        document.querySelector(".step2__button_green").disabled = false;
-      }
-      if (test_step == test.length) {
-        document.querySelector(".buttons-wrap").style.display = "none";
-        document.querySelector(".step2-start").style.display = "none";
-        document.querySelector(".step2-end").style.display = "block";
-      } else {
-        document.querySelector(".question-container[data-id='" + test_step + "']").classList.remove("d-none");
-        document.querySelector(".question-container[data-id='" + test_step + "']").classList.add("d-block");
-        document.querySelector(".step2__button_test-back").disabled = false;
-      }
-    };
-    document.querySelector(".step2__button_test-back").onclick = function () {
-      document.querySelector(".step2__button_green").disabled = false;
-      document.querySelectorAll(".question-container").forEach(function (item, index) {
-        item.classList.remove("d-block");
-        item.classList.add("d-none");
-      });
-      test_step--;
-      document.querySelector(".question-container[data-id='" + test_step + "']").classList.remove("d-none");
-      document.querySelector(".question-container[data-id='" + test_step + "']").classList.add("d-block");
-      if (test_step == 0) {
-        document.querySelector(".step2__button_test-back").disabled = true;
-      } else {
-        document.querySelector(".step2__button_test-back").disabled = false;
-      }
-      document.querySelector(".step2__button_green").textContent = "Следующий вопрос";
-    };
-  }
+  });
 }
+
+document.addEventListener("click", async function (e) {
+  for (let target = e.target; target && target != this; target = target.parentNode) {
+    if (target.matches(".intensive-puzzle_item:not(.opened)")) {
+      const $this = target;
+      const id = $this.getAttribute("data-id");
+      document.querySelectorAll(".question-container").forEach(function (item, index) {
+        item.classList.remove("d-block");
+        item.classList.add("d-none");
+      });
+      document.querySelector(".question-container[data-id='" + id + "']").classList.remove("d-none");
+      document.querySelector(".question-container[data-id='" + id + "']").classList.add("d-block");
+      document.querySelectorAll(".intensive-puzzle_item-first").forEach(function (item, index) {
+        item.classList.remove("intensive-puzzle_item-first_active");
+      });
+      $this.querySelector(".intensive-puzzle_item-first").classList.add("intensive-puzzle_item-first_active");
+    }
+  }
+});
