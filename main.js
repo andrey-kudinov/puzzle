14b44491fc9b78bb1d100c120e6dd2fa9c966040
Andrey Kudinov <2499931@gmail.com>
2021-12-13 23:40:12 +0400

    refactor js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const getTest = async (type = 27) => {
-  let url = `https://opentdb.com/api.php?amount=30&category=${type}&type=multiple`;
+  const url = `https://opentdb.com/api.php?amount=30&category=${type}&type=multiple`;
   const response = await fetch(url, {
     method: "GET",
     mode: "cors",
@@ -8,88 +8,88 @@ const getTest = async (type = 27) => {
     redirect: "follow",
     referrerPolicy: "no-referrer",
   });
-  return await response;
-}
+  const data = await response.json();
+  return data.results;
+};
 
-const shuffle = (array) => array.sort(() => Math.random() - 0.5)
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   let id = 0;
+  const data = await getTest(id)
   document.querySelectorAll(".btn-choose ").forEach((el) => {
     el.onclick = () => {
       id = el.dataset.id;
       document.querySelector(".puzzle__questions").innerHTML = "";
       document.querySelector(".puzzle__result-num").textContent = 1;
-      document.querySelector(".puzzle__progress-caption-js").textContent = `11%`;
-      document.querySelector(".puzzle__progress-bar-green-js").style.width = `11%`;
+      document.querySelector(
+        ".puzzle__progress-caption-js"
+      ).textContent = `11%`;
+      document.querySelector(
+        ".puzzle__progress-bar-green-js"
+      ).style.width = `11%`;
 
       if (document.querySelectorAll(".puzzle__item.opened").length > 1) {
         for (let i = 1; i < 9; i++) {
-          document.querySelectorAll(".puzzle__item")[i].classList.remove("opened");
+          document
+            .querySelectorAll(".puzzle__item")
+            [i].classList.remove("opened");
         }
-
-        setTimeout(() => {
-          getTest(id)
-            .then((response) => {
-              return response.json();
-            })
-            .then((data) => {
-              shuffle(data.results);
-              puzzleStart(data.results);
-            });
-        }, 300);
-      } else {
-        getTest(id)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            shuffle(data.results);
-            puzzleStart(data.results);
-          });
+        puzzleStart(data);
       }
     };
   });
 
-  getTest(27)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      shuffle(data.results);
-      puzzleStart(data.results);
-    });
+  puzzleStart(data);
 
   document.querySelector(".btn-caption").onclick = () => {
-    document.querySelector(".puzzle__buttons").classList.toggle("buttons-wrap_active");
+    document
+      .querySelector(".puzzle__buttons")
+      .classList.toggle("buttons-wrap_active");
   };
   document.addEventListener("mouseup", (e) => {
     if (!document.querySelector(".puzzle__buttons").contains(e.target)) {
-      document.querySelector(".puzzle__buttons").classList.remove("buttons-wrap_active");
+      document
+        .querySelector(".puzzle__buttons")
+        .classList.remove("buttons-wrap_active");
     }
   });
 });
 
-const puzzleStart = async (response) => {
-  let test = response;
+const puzzleStart = async (test) => {
   shuffle(test);
   test = test.slice(0, 8);
 
   let $class = "block";
   let cardClass = "active";
+  const puzzleItemsHTML = document.querySelector(".puzzle__items-js");
+  puzzleItemsHTML.innerHTML = `
+    <div class="puzzle__item opened">
+      <div class="puzzle__item-first active">
+        <div class="puzzle__item-num">0</div>
+      </div>
+      <div class="puzzle__item-second">
+        <img src="./images/1.jpg" alt="" />
+      </div>
+    </div>
+  `;
 
   for (let i = 0; i < test.length; i++) {
-    let answers = '';
+    let answers = "";
     const question = test[i];
-    let answersArray = [test[i].correct_answer, test[i].incorrect_answers[0], test[i].incorrect_answers[1]];
-    
+    let answersArray = [
+      test[i].correct_answer,
+      test[i].incorrect_answers[0],
+      test[i].incorrect_answers[1],
+    ];
+
     if (i !== 0) {
       $class = "hidden";
       cardClass = "";
     }
 
     shuffle(answersArray);
-    
+
     for (let j = 0; j < 3; j++) {
       let answer = answersArray[j];
       // if (answer.correct) {}
@@ -109,16 +109,15 @@ const puzzleStart = async (response) => {
         '<label class="puzzle__input-container-js flex mb-5 cursor-pointer" data-id="' +
         '"><div class="relative flex-shrink-0 box-border w-7 h-7"><input type="radio" name="question' +
         i +
-        '" class="puzzle__input absolute w-1 h-1 m-0 p-0 opacity-0"><div class="answers puzzle__input-check">' + 
+        '" class="puzzle__input absolute w-1 h-1 m-0 p-0 opacity-0"><div class="answers puzzle__input-check">' +
         '</div></div><span class="self-end ml-4 text-gray-800 text-base puzzle__answer">' +
         answer +
         "</span></label>";
 
       answers += answerHTML;
     }
-    
-    const questionHTML = 
-      `<div class="puzzle__question-container ${$class}" data-id="${i}" data-id-backend=${i}>
+
+    const questionHTML = `<div class="puzzle__question-container ${$class}" data-id="${i}" data-id-backend=${i}>
         <div>
           <div class="mb-2 text-yellow-400 text-lg font-bold">
             Вопрос ${i + 1}
@@ -133,11 +132,10 @@ const puzzleStart = async (response) => {
           </div>
         </div>
       </div>`;
-    const questionsHTML = document.querySelector(".puzzle__questions")
+    const questionsHTML = document.querySelector(".puzzle__questions");
     questionsHTML.innerHTML += questionHTML;
 
-    const puzzleItemHTML = 
-      `<div class="puzzle__item" data-id="${i}">
+    const puzzleItemHTML = `<div class="puzzle__item" data-id="${i}">
         <div class="puzzle__item-first ${cardClass}">
           <div class="puzzle__item-num">${i + 1}</div>
         </div>
@@ -145,7 +143,6 @@ const puzzleStart = async (response) => {
           <img src="./images/${i + 2}.jpg" alt="">
         </div>
       </div>`;
-    const puzzleItemsHTML = document.querySelector(".puzzle__items-js")
     puzzleItemsHTML.innerHTML += puzzleItemHTML;
   }
 
@@ -158,24 +155,47 @@ const puzzleStart = async (response) => {
       const question = e.target.closest(".puzzle__question-container");
       const answer = e.target.closest(".puzzle__input-container-js");
       const answer_id = answer.getAttribute("data-id");
-      const question_id = e.target.closest(".puzzle__question-container").getAttribute("data-id");
-      const question_id_backend = e.target.closest(".puzzle__question-container").getAttribute("data-id-backend");
+      const question_id = e.target
+        .closest(".puzzle__question-container")
+        .getAttribute("data-id");
+      const question_id_backend = e.target
+        .closest(".puzzle__question-container")
+        .getAttribute("data-id-backend");
 
       const comment = document.createElement("div");
-      question.querySelectorAll(".puzzle__comment").forEach((item, index) => item.remove());
-      question.querySelectorAll(".puzzle__comment-false-js").forEach((item, index) => item.remove());
+      question
+        .querySelectorAll(".puzzle__comment")
+        .forEach((item, index) => item.remove());
+      question
+        .querySelectorAll(".puzzle__comment-false-js")
+        .forEach((item, index) => item.remove());
       if (answer.textContent == test[question_id].correct_answer) {
         correctAnswersCounter++;
-        document.querySelector(".puzzle__result-num").textContent = correctAnswersCounter;
-        document.querySelector(".puzzle__progress-caption-js").textContent = `${Math.round((correctAnswersCounter * 100) / 9)}%`;
-        document.querySelector(".puzzle__progress-bar-green-js").style.width = `${Math.round((correctAnswersCounter * 100) / 9)}%`;
+        document.querySelector(".puzzle__result-num").textContent =
+          correctAnswersCounter;
+        document.querySelector(
+          ".puzzle__progress-caption-js"
+        ).textContent = `${Math.round((correctAnswersCounter * 100) / 9)}%`;
+        document.querySelector(
+          ".puzzle__progress-bar-green-js"
+        ).style.width = `${Math.round((correctAnswersCounter * 100) / 9)}%`;
         answer.querySelector(".puzzle__input-check").innerHTML =
           '<img class="w-full opacity-0 object-cover" src="./images/input-checked.svg" alt="">';
         comment.classList.add("puzzle__comment");
-        comment.innerHTML = `<div class="mb-2 text-green-600 text-base font-bold">Верно!</div></div></div><div class="puzzle__comment-text">Вопрос уровня ${test[question_id].difficulty}</div>`;
+        comment.innerHTML = 
+          `<div class="mb-2 text-green-600 text-base font-bold">
+            Верно!
+          </div>
+          <div class="puzzle__comment-text">
+            Вопрос уровня ${test[question_id].difficulty}
+          </div>`;
         answer.classList.add("puzzle__input-container-correct");
-        question.querySelectorAll(".puzzle__input-container-js").forEach((item, index) => item.classList.add("pointer-events-none"));
-        document.querySelector(".puzzle__item[data-id='" + question_id + "']").classList.add("opened");
+        question
+          .querySelectorAll(".puzzle__input-container-js")
+          .forEach((item, index) => item.classList.add("pointer-events-none"));
+        document
+          .querySelector(".puzzle__item[data-id='" + question_id + "']")
+          .classList.add("opened");
         if (correctAnswersCounter == 9) {
           document.querySelector(".puzzle__result-all").style.color = "#061b36";
         }
@@ -191,25 +211,39 @@ const puzzleStart = async (response) => {
           '<img class="w-full opacity-0 object-cover" src="./images/input-error.svg" alt="">';
         comment.innerText = `Попробуй еще раз!`;
         comment.classList.add("puzzle__comment-false-js");
-        document.querySelectorAll(".puzzle__input-container-js").forEach((item, index) => item.classList.remove("text-red-600"));
+        document
+          .querySelectorAll(".puzzle__input-container-js")
+          .forEach((item, index) => item.classList.remove("text-red-600"));
         answer.classList.add("text-red-600");
       }
       answer.after(comment);
     });
   });
-}
+};
 
 document.addEventListener("click", async function (e) {
-  for (let target = e.target; target && target != this; target = target.parentNode) {
+  for (
+    let target = e.target;
+    target && target != this;
+    target = target.parentNode
+  ) {
     if (target.matches(".puzzle__item:not(.opened)")) {
       const id = target.getAttribute("data-id");
-      document.querySelectorAll(".puzzle__question-container").forEach((item, index) => {
-        item.classList.remove("block");
-        item.classList.add("hidden");
-      });
-      document.querySelector(".puzzle__question-container[data-id='" + id + "']").classList.remove("hidden");
-      document.querySelector(".puzzle__question-container[data-id='" + id + "']").classList.add("block");
-      document.querySelectorAll(".puzzle__item-first").forEach((item, index) => item.classList.remove("active"));
+      document
+        .querySelectorAll(".puzzle__question-container")
+        .forEach((item, index) => {
+          item.classList.remove("block");
+          item.classList.add("hidden");
+        });
+      document
+        .querySelector(".puzzle__question-container[data-id='" + id + "']")
+        .classList.remove("hidden");
+      document
+        .querySelector(".puzzle__question-container[data-id='" + id + "']")
+        .classList.add("block");
+      document
+        .querySelectorAll(".puzzle__item-first")
+        .forEach((item, index) => item.classList.remove("active"));
       target.querySelector(".puzzle__item-first").classList.add("active");
     }
   }
